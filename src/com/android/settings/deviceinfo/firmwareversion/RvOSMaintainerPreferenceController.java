2501951e5e7019672be94cb79d31cd3f388841a4
Rve27 <rve27github@gmail.com>
2024-11-23 06:09:21 +0800

    Settings: Open maintainer profile link if maintainer name is clicked
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/RvOSMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/RvOSMaintainerPreferenceController.java
@@ -17,7 +17,13 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.preference.Preference;
 
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
@@ -25,12 +31,16 @@ import com.android.settings.core.BasePreferenceController;
 public class RvOSMaintainerPreferenceController extends BasePreferenceController {
 
     private static final String RVOS_MAINTAINER = "ro.rvos.maintainer";
+    private static final String RVOS_MAINTAINER_LINK = "ro.rvos.maintainer.link";
+    private static final String TAG = "RvOSMaintainerPrefCtrl";
 
     private final Context mContext;
+    private final PackageManager mPackageManager;
 
     public RvOSMaintainerPreferenceController(Context context, String key) {
         super(context, key);
         mContext = context;
+        mPackageManager = context.getPackageManager();
     }
 
     @Override
@@ -42,6 +52,31 @@ public class RvOSMaintainerPreferenceController extends BasePreferenceController
     public CharSequence getSummary() {
         String rvosMaintainer = SystemProperties.get(RVOS_MAINTAINER,
             mContext.getResources().getString(R.string.device_info_default));
-		return rvosMaintainer;
+        return rvosMaintainer;
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
+            return false;
+        }
+
+        String maintainerLink = SystemProperties.get(RVOS_MAINTAINER_LINK, null);
+        if (TextUtils.isEmpty(maintainerLink)) {
+            Log.w(TAG, "Maintainer link is empty or undefined");
+            return true;
+        }
+
+        final Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(android.net.Uri.parse(maintainerLink));
+
+        if (mPackageManager.queryIntentActivities(intent, 0).isEmpty()) {
+            Log.w(TAG, "queryIntentActivities() returns empty");
+            return true;
+        }
+
+        mContext.startActivity(intent);
+        return true;
     }
 }
