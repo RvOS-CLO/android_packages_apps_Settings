09fc34ad9c016780c72404f3036ebbe22d112397
Rve27 <rve27github@gmail.com>
2024-11-23 06:38:07 +0800

    Settings: Remove logging maintainer profile link
    because the default value is already defined here
    https://github.com/RvOS-CLO/android_vendor_rvos/commit/66131483f419a220e45c7007bc34d555bee66205
    
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/RvOSMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/RvOSMaintainerPreferenceController.java
@@ -21,7 +21,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.SystemProperties;
 import android.text.TextUtils;
-import android.util.Log;
 
 import androidx.preference.Preference;
 
@@ -32,7 +31,6 @@ public class RvOSMaintainerPreferenceController extends BasePreferenceController
 
     private static final String RVOS_MAINTAINER = "ro.rvos.maintainer";
     private static final String RVOS_MAINTAINER_LINK = "ro.rvos.maintainer.link";
-    private static final String TAG = "RvOSMaintainerPrefCtrl";
 
     private final Context mContext;
     private final PackageManager mPackageManager;
@@ -61,20 +59,11 @@ public class RvOSMaintainerPreferenceController extends BasePreferenceController
             return false;
         }
 
-        String maintainerLink = SystemProperties.get(RVOS_MAINTAINER_LINK, null);
-        if (TextUtils.isEmpty(maintainerLink)) {
-            Log.w(TAG, "Maintainer link is empty or undefined");
-            return true;
-        }
+        String maintainerLink = SystemProperties.get(RVOS_MAINTAINER_LINK);
 
         final Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
         intent.setData(android.net.Uri.parse(maintainerLink));
-
-        if (mPackageManager.queryIntentActivities(intent, 0).isEmpty()) {
-            Log.w(TAG, "queryIntentActivities() returns empty");
-            return true;
-        }
 
         mContext.startActivity(intent);
         return true;
